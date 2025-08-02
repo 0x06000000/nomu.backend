34fc7de5431e21520ae8e50bee430ceb4cb0e9a4
H0-M3 <home@sangsangsoft.com>
2025-08-02 13:13:43 +0900

    fix

---
+++ .wrangler/tmp/dev-lPbhE8/worker.js
@@ -0,0 +1,453 @@
+(() => {
+  var __defProp = Object.defineProperty;
+  var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
+
+  // .wrangler/tmp/bundle-jqZrb5/checked-fetch.js
+  var urls = /* @__PURE__ */ new Set();
+  function checkURL(request, init) {
+    const url = request instanceof URL ? request : new URL(
+      (typeof request === "string" ? new Request(request, init) : request).url
+    );
+    if (url.port && url.port !== "443" && url.protocol === "https:") {
+      if (!urls.has(url.toString())) {
+        urls.add(url.toString());
+        console.warn(
+          `WARNING: known issue with \`fetch()\` requests to custom HTTPS ports in published Workers:
+ - ${url.toString()} - the custom port will be ignored when the Worker is published using the \`wrangler deploy\` command.
+`
+        );
+      }
+    }
+  }
+  __name(checkURL, "checkURL");
+  globalThis.fetch = new Proxy(globalThis.fetch, {
+    apply(target, thisArg, argArray) {
+      const [request, init] = argArray;
+      checkURL(request, init);
+      return Reflect.apply(target, thisArg, argArray);
+    }
+  });
+
+  // ../../../AppData/Local/npm-cache/_npx/32026684e21afda6/node_modules/wrangler/templates/middleware/common.ts
+  var __facade_middleware__ = [];
+  function __facade_register__(...args) {
+    __facade_middleware__.push(...args.flat());
+  }
+  __name(__facade_register__, "__facade_register__");
+  function __facade_registerInternal__(...args) {
+    __facade_middleware__.unshift(...args.flat());
+  }
+  __name(__facade_registerInternal__, "__facade_registerInternal__");
+  function __facade_invokeChain__(request, env, ctx, dispatch, middlewareChain) {
+    const [head, ...tail] = middlewareChain;
+    const middlewareCtx = {
+      dispatch,
+      next(newRequest, newEnv) {
+        return __facade_invokeChain__(newRequest, newEnv, ctx, dispatch, tail);
+      }
+    };
+    return head(request, env, ctx, middlewareCtx);
+  }
+  __name(__facade_invokeChain__, "__facade_invokeChain__");
+  function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
+    return __facade_invokeChain__(request, env, ctx, dispatch, [
+      ...__facade_middleware__,
+      finalMiddleware
+    ]);
+  }
+  __name(__facade_invoke__, "__facade_invoke__");
+
+  // ../../../AppData/Local/npm-cache/_npx/32026684e21afda6/node_modules/wrangler/templates/middleware/loader-sw.ts
+  var __FACADE_EVENT_TARGET__;
+  if (globalThis.MINIFLARE) {
+    __FACADE_EVENT_TARGET__ = new (Object.getPrototypeOf(WorkerGlobalScope))();
+  } else {
+    __FACADE_EVENT_TARGET__ = new EventTarget();
+  }
+  function __facade_isSpecialEvent__(type) {
+    return type === "fetch" || type === "scheduled";
+  }
+  __name(__facade_isSpecialEvent__, "__facade_isSpecialEvent__");
+  var __facade__originalAddEventListener__ = globalThis.addEventListener;
+  var __facade__originalRemoveEventListener__ = globalThis.removeEventListener;
+  var __facade__originalDispatchEvent__ = globalThis.dispatchEvent;
+  globalThis.addEventListener = function(type, listener, options) {
+    if (__facade_isSpecialEvent__(type)) {
+      __FACADE_EVENT_TARGET__.addEventListener(
+        type,
+        listener,
+        options
+      );
+    } else {
+      __facade__originalAddEventListener__(type, listener, options);
+    }
+  };
+  globalThis.removeEventListener = function(type, listener, options) {
+    if (__facade_isSpecialEvent__(type)) {
+      __FACADE_EVENT_TARGET__.removeEventListener(
+        type,
+        listener,
+        options
+      );
+    } else {
+      __facade__originalRemoveEventListener__(type, listener, options);
+    }
+  };
+  globalThis.dispatchEvent = function(event) {
+    if (__facade_isSpecialEvent__(event.type)) {
+      return __FACADE_EVENT_TARGET__.dispatchEvent(event);
+    } else {
+      return __facade__originalDispatchEvent__(event);
+    }
+  };
+  globalThis.addMiddleware = __facade_register__;
+  globalThis.addMiddlewareInternal = __facade_registerInternal__;
+  var __facade_waitUntil__ = Symbol("__facade_waitUntil__");
+  var __facade_response__ = Symbol("__facade_response__");
+  var __facade_dispatched__ = Symbol("__facade_dispatched__");
+  var __Facade_ExtendableEvent__ = class ___Facade_ExtendableEvent__ extends Event {
+    static {
+      __name(this, "__Facade_ExtendableEvent__");
+    }
+    [__facade_waitUntil__] = [];
+    waitUntil(promise) {
+      if (!(this instanceof ___Facade_ExtendableEvent__)) {
+        throw new TypeError("Illegal invocation");
+      }
+      this[__facade_waitUntil__].push(promise);
+    }
+  };
+  var __Facade_FetchEvent__ = class ___Facade_FetchEvent__ extends __Facade_ExtendableEvent__ {
+    static {
+      __name(this, "__Facade_FetchEvent__");
+    }
+    #request;
+    #passThroughOnException;
+    [__facade_response__];
+    [__facade_dispatched__] = false;
+    constructor(type, init) {
+      super(type);
+      this.#request = init.request;
+      this.#passThroughOnException = init.passThroughOnException;
+    }
+    get request() {
+      return this.#request;
+    }
+    respondWith(response) {
+      if (!(this instanceof ___Facade_FetchEvent__)) {
+        throw new TypeError("Illegal invocation");
+      }
+      if (this[__facade_response__] !== void 0) {
+        throw new DOMException(
+          "FetchEvent.respondWith() has already been called; it can only be called once.",
+          "InvalidStateError"
+        );
+      }
+      if (this[__facade_dispatched__]) {
+        throw new DOMException(
+          "Too late to call FetchEvent.respondWith(). It must be called synchronously in the event handler.",
+          "InvalidStateError"
+        );
+      }
+      this.stopImmediatePropagation();
+      this[__facade_response__] = response;
+    }
+    passThroughOnException() {
+      if (!(this instanceof ___Facade_FetchEvent__)) {
+        throw new TypeError("Illegal invocation");
+      }
+      this.#passThroughOnException();
+    }
+  };
+  var __Facade_ScheduledEvent__ = class ___Facade_ScheduledEvent__ extends __Facade_ExtendableEvent__ {
+    static {
+      __name(this, "__Facade_ScheduledEvent__");
+    }
+    #scheduledTime;
+    #cron;
+    #noRetry;
+    constructor(type, init) {
+      super(type);
+      this.#scheduledTime = init.scheduledTime;
+      this.#cron = init.cron;
+      this.#noRetry = init.noRetry;
+    }
+    get scheduledTime() {
+      return this.#scheduledTime;
+    }
+    get cron() {
+      return this.#cron;
+    }
+    noRetry() {
+      if (!(this instanceof ___Facade_ScheduledEvent__)) {
+        throw new TypeError("Illegal invocation");
+      }
+      this.#noRetry();
+    }
+  };
+  __facade__originalAddEventListener__("fetch", (event) => {
+    const ctx = {
+      waitUntil: event.waitUntil.bind(event),
+      passThroughOnException: event.passThroughOnException.bind(event)
+    };
+    const __facade_sw_dispatch__ = /* @__PURE__ */ __name(function(type, init) {
+      if (type === "scheduled") {
+        const facadeEvent = new __Facade_ScheduledEvent__("scheduled", {
+          scheduledTime: Date.now(),
+          cron: init.cron ?? "",
+          noRetry() {
+          }
+        });
+        __FACADE_EVENT_TARGET__.dispatchEvent(facadeEvent);
+        event.waitUntil(Promise.all(facadeEvent[__facade_waitUntil__]));
+      }
+    }, "__facade_sw_dispatch__");
+    const __facade_sw_fetch__ = /* @__PURE__ */ __name(function(request, _env, ctx2) {
+      const facadeEvent = new __Facade_FetchEvent__("fetch", {
+        request,
+        passThroughOnException: ctx2.passThroughOnException
+      });
+      __FACADE_EVENT_TARGET__.dispatchEvent(facadeEvent);
+      facadeEvent[__facade_dispatched__] = true;
+      event.waitUntil(Promise.all(facadeEvent[__facade_waitUntil__]));
+      const response = facadeEvent[__facade_response__];
+      if (response === void 0) {
+        throw new Error("No response!");
+      }
+      return response;
+    }, "__facade_sw_fetch__");
+    event.respondWith(
+      __facade_invoke__(
+        event.request,
+        globalThis,
+        ctx,
+        __facade_sw_dispatch__,
+        __facade_sw_fetch__
+      )
+    );
+  });
+  __facade__originalAddEventListener__("scheduled", (event) => {
+    const facadeEvent = new __Facade_ScheduledEvent__("scheduled", {
+      scheduledTime: event.scheduledTime,
+      cron: event.cron,
+      noRetry: event.noRetry.bind(event)
+    });
+    __FACADE_EVENT_TARGET__.dispatchEvent(facadeEvent);
+    event.waitUntil(Promise.all(facadeEvent[__facade_waitUntil__]));
+  });
+
+  // ../../../AppData/Local/npm-cache/_npx/32026684e21afda6/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts
+  var drainBody = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx) => {
+    try {
+      return await middlewareCtx.next(request, env);
+    } finally {
+      try {
+        if (request.body !== null && !request.bodyUsed) {
+          const reader = request.body.getReader();
+          while (!(await reader.read()).done) {
+          }
+        }
+      } catch (e) {
+        console.error("Failed to drain the unused request body.", e);
+      }
+    }
+  }, "drainBody");
+  var middleware_ensure_req_body_drained_default = drainBody;
+
+  // ../../../AppData/Local/npm-cache/_npx/32026684e21afda6/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts
+  function reduceError(e) {
+    return {
+      name: e?.name,
+      message: e?.message ?? String(e),
+      stack: e?.stack,
+      cause: e?.cause === void 0 ? void 0 : reduceError(e.cause)
+    };
+  }
+  __name(reduceError, "reduceError");
+  var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx) => {
+    try {
+      return await middlewareCtx.next(request, env);
+    } catch (e) {
+      const error = reduceError(e);
+      return Response.json(error, {
+        status: 500,
+        headers: { "MF-Experimental-Error-Stack": "true" }
+      });
+    }
+  }, "jsonError");
+  var middleware_miniflare3_json_error_default = jsonError;
+
+  // .wrangler/tmp/bundle-jqZrb5/middleware-insertion-facade.js
+  __facade_registerInternal__([middleware_ensure_req_body_drained_default, middleware_miniflare3_json_error_default]);
+
+  // worker.js
+  var API_BASE_URL = "https://apis.data.go.kr/B490001/gySjbPstateInfoService";
+  var \uC0AC\uC5C5\uC7A5\uC7A5\uC870\uD68C_API_URL = `${API_BASE_URL}/getGySjBoheomBsshItem`;
+  var SERVICE_KEY = "iaxjwmL9Hxpw3MSZ6XSYR0wcx1bWX0+18BmyAuILHPob+Qjn/F+Vt3sez2SsejjveYC0Ck+4EsAENKZ6JB2jTA==";
+  addEventListener("fetch", (event) => {
+    event.respondWith(handleRequest(event.request));
+  });
+  async function handleRequest(request) {
+    const url = new URL(request.url);
+    const path = url.pathname;
+    const corsHeaders = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Content-Type": "application/json;charset=UTF-8"
+    };
+    if (request.method === "OPTIONS") {
+      return new Response(null, {
+        status: 200,
+        headers: corsHeaders
+      });
+    }
+    if (request.method !== "GET") {
+      return new Response(JSON.stringify({ error: "Method not allowed" }), {
+        status: 405,
+        headers: corsHeaders
+      });
+    }
+    if (path === "/health") {
+      return handleHealthCheck();
+    }
+    if (path === "/api/business") {
+      return handleBusinessAPI(url, corsHeaders);
+    }
+    return new Response(JSON.stringify({
+      error: "Not found",
+      availableEndpoints: ["/health", "/api/business"]
+    }), {
+      status: 404,
+      headers: corsHeaders
+    });
+  }
+  __name(handleRequest, "handleRequest");
+  async function handleBusinessAPI(url, corsHeaders) {
+    try {
+      const searchParams = url.searchParams;
+      const businessNumber = searchParams.get("businessNumber");
+      const pageNo = searchParams.get("pageNo") || "1";
+      const numOfRows = searchParams.get("numOfRows") || "10";
+      const opaBoheomFg = searchParams.get("opaBoheomFg") || "1";
+      if (!businessNumber) {
+        return new Response(JSON.stringify({
+          error: "\uC0AC\uC5C5\uC790\uB4F1\uB85D\uBC88\uD638(businessNumber)\uB294 \uD544\uC218\uC785\uB2C8\uB2E4."
+        }), {
+          status: 400,
+          headers: corsHeaders
+        });
+      }
+      const apiUrl = new URL(\uC0AC\uC5C5\uC7A5\uC7A5\uC870\uD68C_API_URL);
+      const encodedServiceKey = encodeURIComponent(SERVICE_KEY);
+      apiUrl.searchParams.set("serviceKey", encodedServiceKey);
+      apiUrl.searchParams.set("v_saeopjaDrno", businessNumber);
+      apiUrl.searchParams.set("opaBoheomFg", opaBoheomFg);
+      apiUrl.searchParams.set("pageNo", pageNo);
+      apiUrl.searchParams.set("numOfRows", numOfRows);
+      console.log(1111111111111111, apiUrl.toString());
+      const apiResponse = await fetch(apiUrl.toString(), {
+        headers: {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        }
+      });
+      if (!apiResponse.ok) {
+        const errorText = await apiResponse.text();
+        console.error("API Error Response:", errorText);
+        throw new Error(`API request failed: ${apiResponse.status} ${apiResponse.statusText} - ${errorText}`);
+      }
+      const apiData = await apiResponse.text();
+      const jsonData = await parseXMLResponse(apiData);
+      const response = new Response(JSON.stringify(jsonData), {
+        status: 200,
+        headers: corsHeaders
+      });
+      return response;
+    } catch (error) {
+      console.error("Worker error:", error);
+      return new Response(JSON.stringify({
+        error: "\uC11C\uBC84 \uC624\uB958\uAC00 \uBC1C\uC0DD\uD588\uC2B5\uB2C8\uB2E4.",
+        details: error.message
+      }), {
+        status: 500,
+        headers: corsHeaders
+      });
+    }
+  }
+  __name(handleBusinessAPI, "handleBusinessAPI");
+  async function parseXMLResponse(xmlString) {
+    try {
+      const responseMatch = xmlString.match(/<responseBody>(.*?)<\/responseBody>/s);
+      if (!responseMatch) {
+        throw new Error("Invalid XML response format");
+      }
+      const responseBody = responseMatch[1];
+      const items = [];
+      const itemRegex = /<item>(.*?)<\/item>/gs;
+      let itemMatch;
+      while ((itemMatch = itemRegex.exec(responseBody)) !== null) {
+        const itemXml = itemMatch[1];
+        const item = {};
+        const fieldRegex = /<([^>]+)>([^<]*)<\/\1>/g;
+        let fieldMatch;
+        while ((fieldMatch = fieldRegex.exec(itemXml)) !== null) {
+          const fieldName = fieldMatch[1];
+          const fieldValue = fieldMatch[2];
+          item[fieldName] = fieldValue;
+        }
+        items.push(item);
+      }
+      const headerMatch = responseBody.match(/<header>(.*?)<\/header>/s);
+      let header = {};
+      if (headerMatch) {
+        const headerXml = headerMatch[1];
+        const fieldRegex = /<([^>]+)>([^<]*)<\/\1>/g;
+        let fieldMatch;
+        while ((fieldMatch = fieldRegex.exec(headerXml)) !== null) {
+          const fieldName = fieldMatch[1];
+          const fieldValue = fieldMatch[2];
+          header[fieldName] = fieldValue;
+        }
+      }
+      return {
+        header,
+        items,
+        totalCount: items.length,
+        cachedAt: (/* @__PURE__ */ new Date()).toISOString()
+      };
+    } catch (error) {
+      console.error("XML parsing error:", error);
+      return {
+        error: "XML \uD30C\uC2F1 \uC624\uB958",
+        originalResponse: xmlString,
+        details: error.message
+      };
+    }
+  }
+  __name(parseXMLResponse, "parseXMLResponse");
+  async function handleHealthCheck() {
+    return new Response(JSON.stringify({
+      status: "healthy",
+      service: "\uADFC\uB85C\uBCF5\uC9C0\uACF5\uB2E8 API Proxy Worker",
+      timestamp: (/* @__PURE__ */ new Date()).toISOString(),
+      endpoints: {
+        "/api/business": "\uC0AC\uC5C5\uC790 \uC815\uBCF4 \uC870\uD68C (GET)",
+        "/health": "\uD5EC\uC2A4 \uCCB4\uD06C (GET)"
+      },
+      parameters: {
+        businessNumber: "\uC0AC\uC5C5\uC790\uB4F1\uB85D\uBC88\uD638 (\uD544\uC218)",
+        opaBoheomFg: "\uC0B0\uC7AC/\uACE0\uC6A9 \uAD6C\uBD84 (1: \uC0B0\uC7AC, 2: \uACE0\uC6A9)",
+        pageNo: "\uD398\uC774\uC9C0 \uBC88\uD638",
+        numOfRows: "\uBAA9\uB85D \uAC74\uC218"
+      }
+    }), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+  }
+  __name(handleHealthCheck, "handleHealthCheck");
+})();
+//# sourceMappingURL=worker.js.map
