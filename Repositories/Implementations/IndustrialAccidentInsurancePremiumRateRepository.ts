6088d42d18a4d79f95f9edf87d5899e60cf87c14
H0-M3 <home@sangsangsoft.com>
2025-08-25 01:00:52 +0900

    fix

--- Repositories/Implementations/IndustrialAccidentInsurancePremiumRateRepository.ts
+++ Repositories/Implementations/IndustrialAccidentInsurancePremiumRateRepository.ts
@@ -20,7 +20,7 @@ export class IndustrialAccidentInsurancePremiumRateRepository implements IIndust
         date: string;
         rate: number;
     }[]): Promise<void> {
-        const batchSize = 500;
+        const batchSize = 12;
 
         for (let i = 0; i < industrialAccidentInsurancePremiumRates.length; i += batchSize) {
             const batch = industrialAccidentInsurancePremiumRates.slice(i, i + batchSize);
